60f79984aaaef598ab665b63e3a14597b9f3ca3e
Scott Serok <sserok@grubhub.com>
2017-04-20 15:58:23 -0400

    remove codebuild since we use a docker image build during deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-tmp_dir=/tmp/app
 deploy_dir=/app
-archive_dir=/archives
 echo $(date) - Starting after_install.sh
-echo Moving files in $deploy_dir to $archive_dir
-mv $deploy_dir/* $archive_dir
-echo $(ls $deploy_dir)
-echo $(ls $archive_dir)
-echo Extracting from $tmp_dir into $deploy_dir
-tar xzvf $tmp_dir
-mv $tmp_dir/*.tar.gz $deploy_dir
-echo Docker load
-docker load < $deploy_dir/*tar.gz
-echo $(docker ps)
+cd $deploy_dir && docker build -t $DEPLOYMENT_ID .
 echo $(date) - Finished after_install.sh
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 deploy_dir="/app"
 echo $(date) - Starting application_start.sh
-cd $deploy_dir
-docker run -d --name appcontainer -p 80:80 appimage
+cd $deploy_dir && docker run -d --name appcontainer -p 80:80 $DEPLOYMENT_ID
 echo $(date) - Finished application_start.sh
